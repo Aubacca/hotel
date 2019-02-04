e959847b33e0a2265ee5a1cb22e434d59f9b6af2
Aubacca Ubuntu <aubacca.renhor@gmail.com>
2019-02-04 08:56:54 +0100

    First services added.
    * hotel service
    * section service (still under construction)
    * unit test added

---
+++ src/main/java/ch/renhor/hotel/services/SectionService.java
@@ -0,0 +1,17 @@
+package ch.renhor.hotel.services;
+
+import ch.renhor.hotel.models.Hotel;
+import ch.renhor.hotel.models.Section;
+
+import java.util.List;
+
+public class SectionService {
+    private List<Section> sectionList;
+
+    public SectionService() {
+    }
+
+//    public List<Section> findAll(Hotel hotel) {
+//        return sectionList.stream().filter(section -> section.getHotel().equals(hotel.getName())).map()
+//    }
+}
